489465fcefe2a0dad3e23f3577ae6a4807014151
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 13:05:30 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <myname v-for="i in 3"></myname>
+  </div>  
+</template>
+
+<script>
+import Homy from './assets/Components/Home'
+export default {
+  components:{
+    myname : Homy
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/assets/Components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Name: {{myname}}</h1>
+        <h1>how are u??</h1>
+        <button @click="changeit">change name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            myname : "kartik"
+        }
+    },
+    methods: {
+        changeit(){
+            this.myname = "chirag";
+        }
+    }    
+}
+</script>
